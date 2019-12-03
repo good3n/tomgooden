0316ad833b7dcb41ce0d9263c2207044fee642e0
Tom Gooden <good3n@gmail.com>
2019-12-03 14:15:38 -0500

    include local fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/src/assets/fonts`
+      }
+    },
     `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,

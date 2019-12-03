96ab129210bdcf8c497a5841b294a65bc0f7bdad
Tom Gooden <good3n@gmail.com>
2019-12-03 17:39:51 -0500

    update site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
@@ -66,6 +66,14 @@ module.exports = {
               target: "_self",
               rel: "nofollow"
             }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              quality: 70,
+              maxWidth: 670,
+              wrapperStyle: `display: block;`
+            },
           },
         ],
       },

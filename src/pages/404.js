c3eff40d66de19d015e5f7c12e5656f684cbb3bc
Tom Gooden <good3n@gmail.com>
2019-12-07 08:00:45 -0500

    update layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,38 +4,56 @@ import SEO from "../components/seo"
 import styled from "styled-components"
 import Layout from "../components/layout"
 
-const Title = styled.h1`
-  font-size: 150px;
-  font-weight: 700;
-  line-height: 1;
-  margin: 0 0 20px;
-  letter-spacing: -5px;
-`
+const Wrapper = styled.div`
+  min-height: calc(100vh - 255px);
+  display: flex;
+  align-items: center;
 
-const Text = styled.p`
-  font-weight: 300;
-  font-size: 28px;
-  line-height: 1.6;
-  color: #777;
-  margin-bottom: 40px;
+  @media (max-width: 899px) {
+    min-height: calc(100vh - 272px);
+  }
+
+  h1 {
+    font-size: 150px;
+    font-weight: 700;
+    line-height: 1;
+    margin: 0 0 20px;
+    letter-spacing: -5px;
+  }
+
+  p {
+    font-weight: 300;
+    font-size: 21px;
+    line-height: 1.6;
+    color: #777;
+    margin-bottom: 40px;
+  }
+
+  svg {
+    position: relative;
+    top: 8px;
+  }
 `
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 Error - Page Not Found" />
-    <Title>404</Title>
-    <Text>Insert corny semi-creative 404 page text here. ¯\_(ツ)_/¯</Text>
-    <Link to="/" style={{
-      display: `flex`,
-      alignItems: `center`,
-      textDecoration: `none`,
-      color: `#121212`,
-      fontWeight: `700`
-    }}>
-      <svg style={{ width: `32px`, marginRight: `10px` }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 904 904">
-        <path fill="#121212" d="M0 452l327 229V537h577V367H327V223z" />
-      </svg>
-      <span>Back Home</span></Link>
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <p>Insert corny semi-creative 404 page text here. ¯\_(ツ)_/¯</p>
+        <Link to="/" style={{
+          display: `inline-block`,
+          textDecoration: `none`,
+          color: `#121212`,
+          fontWeight: `700`
+        }}>
+          <svg style={{ width: `32px`, marginRight: `10px` }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 904 904">
+            <path fill="#121212" d="M0 452l327 229V537h577V367H327V223z" />
+          </svg>
+          <span>Back Home</span></Link>
+      </div>
+    </Wrapper>
   </Layout>
 )
 

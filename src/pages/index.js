e45d58b084a35544517f2ed44a69a7f27e8167ea
Tom <good3n@gmail.com>
2021-02-15 23:43:40 -0500

    Update project roadmap section

--- src/pages/index.js
+++ src/pages/index.js
@@ -143,9 +143,11 @@ const IndexPage = ({ data }) => {
         <div>
           <h2>Project roadmap</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation
+            The "4 D's" as some call it, a common approach to deciphering the
+            lifecycle of a website or application project. However more often
+            than not, it's become lip service. That's not the case here.
+            Planning is everything. Without implementing and executing a real
+            plan of action, your project will begin to take on water.
           </p>
         </div>
         <div className="container">
@@ -154,29 +156,34 @@ const IndexPage = ({ data }) => {
             <li>
               <h3>1. Discover</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt.
+                To determine the real scope of the work, we'll work closely
+                together to learn your goals and pain points. This step is
+                important to ensure we identify what problems we will be
+                solving.
               </p>
             </li>
             <li>
               <h3>2. Design</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt.
+                Based on the findings of discovery, your project will be planned
+                from start to finish. Deliverable timelines will be established,
+                as well as initial concepts created.
               </p>
             </li>
             <li>
               <h3>3. Develop</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt.
+                With the planning done, it's time to start bringing these ideas
+                to life. Each project has a strict development process to ensure
+                quality is met.
               </p>
             </li>
             <li>
               <h3>4. Deploy</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt.
+                The final phase in the roadmap. Your application will be tested
+                and then deployed. This isn't the end of the overall journey
+                however. Digital tools require ongoing attention and care.
               </p>
             </li>
           </ul>

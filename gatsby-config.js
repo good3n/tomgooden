769cc560071fc978f6f1e157b7b0d9055387c569
Tom <good3n@gmail.com>
2021-02-26 02:15:44 -0500

    Remove gatsby plugin offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,8 +49,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `tom-gooden-website`,
+        short_name: `tom-gooden-website`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -58,9 +58,6 @@ module.exports = {
         icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {

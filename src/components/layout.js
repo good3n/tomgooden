b8d8e0eec66aa0b9d610ec108d2a4db311b3584f
Tom <good3n@gmail.com>
2021-02-26 10:22:43 -0500

    Remove react spring animation on page load

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import GlobalStyle from '../assets/styles/global'
 import Helmet from 'react-helmet'
-import { useSpring, animated } from 'react-spring'
 import { Header } from './Header'
 import { Footer } from './Footer'
 
 const Layout = ({ children }) => {
   const [theme, setTheme] = useState('light')
-  const transitions = useSpring({ opacity: 1, from: { opacity: 0 } })
 
   const toggleTheme = () => {
     setTheme(theme === `light` ? `dark` : `light`)
@@ -23,7 +21,7 @@ const Layout = ({ children }) => {
         }}
       />
       <Header theme={theme} toggleTheme={toggleTheme} />
-      <animated.main style={transitions}>{children}</animated.main>
+      <main>{children}</main>
       <Footer />
     </>
   )

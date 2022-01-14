2a038026fd77da06ea806c6f26c882b2adff8a54
Tom Gooden <good3n@gmail.com>
2022-01-14 11:48:08 -0500

    Eslint updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,106 +1,102 @@
 import React from 'react'
-import Seo from '../components/Seo'
 import ReactTypingEffect from 'react-typing-effect'
-import Layout from '../components/Layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
 import { Home } from '../assets/styles/pages/Index.styles'
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <Seo
-        title="Web Developer / Designer"
-        description="I'm a Michigan based Developer and Designer who specializes in custom solutions for businesses including internal tooling a customer facing applications."
-      />
-      <Home>
-        <div className="container">
-          <div>
-            <div className="type">
-              <ReactTypingEffect
-                text={[
-                  "import { Development } from 'tom-gooden'",
-                  "import { Design } from 'tom-gooden'",
-                  "import { Seo } from 'tom-gooden'",
-                ]}
-                speed={100}
-                eraseSpeed={30}
-              />
-            </div>
-            <h2>
-              Digital <br />
-              Thing-maker
-            </h2>
-            <nav>
-              <Link to="/about">About</Link>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 16 16"
-              >
-                <path fill="#F52D86" d="M0 0h16v16H0z" />
-              </svg>
-              <Link to="/uses">Uses</Link>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 16 16"
-              >
-                <circle cx="8" cy="8" r="8" fill="#53CAEE" />
-              </svg>
-              <Link to="/projects">Projects</Link>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 15"
-              >
-                <path fill="#711CB4" d="m8.24 0 8.17 14.46H.21L8.24 0Z" />
-              </svg>
-              <Link to="/contact">Contact</Link>
-            </nav>
-            <h1>
-              I'm a Michigan based Developer and Designer who specializes in
-              custom solutions for businesses including internal tooling a
-              customer facing applications.
-            </h1>
-          </div>
-          <div className="image-wrap">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 657 549"
-            >
-              <path
-                fill="#711CB4"
-                fillOpacity=".1"
-                d="m271 163 386 107.89L380.6 549 271 163Z"
-              />
-              <path fill="#53CAEE" fillOpacity=".1" d="M0 28h268v268H0z" />
-              <circle
-                cx="313.5"
-                cy="216.5"
-                r="216.5"
-                fill="#F72585"
-                fillOpacity=".2"
-              />
-            </svg>
-            <StaticImage
-              src="../assets/images/tom-gooden-web-design-development.png"
-              alt="Tom Gooden"
-              placeholder="tracedSVG"
-              aspectRatio={1 / 1}
-              width={499}
-              imgStyle={{
-                filter: `grayscale(100%) contrast(150%)`,
-              }}
-              // objectFit="cover"
-              // objectPosition="50% 50%"
+const IndexPage = () => (
+  <Layout>
+    <Seo
+      description="I'm a Michigan based Developer and Designer who specializes in custom solutions for businesses including internal tooling a customer facing applications."
+      title="Web Developer / Designer"
+    />
+    <Home>
+      <div className="container">
+        <div>
+          <div className="type">
+            <ReactTypingEffect
+              eraseSpeed={30}
+              speed={100}
+              text={[
+                "import { Development } from 'tom-gooden'",
+                "import { Design } from 'tom-gooden'",
+                "import { Seo } from 'tom-gooden'",
+              ]}
             />
           </div>
+          <h2>
+            Digital <br />
+            Thing-maker
+          </h2>
+          <nav>
+            <Link to="/about">About</Link>
+            <svg
+              fill="none"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="#F52D86" d="M0 0h16v16H0z" />
+            </svg>
+            <Link to="/uses">Uses</Link>
+            <svg
+              fill="none"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="8" cy="8" r="8" fill="#53CAEE" />
+            </svg>
+            <Link to="/projects">Projects</Link>
+            <svg
+              fill="none"
+              viewBox="0 0 17 15"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="#711CB4" d="m8.24 0 8.17 14.46H.21L8.24 0Z" />
+            </svg>
+            <Link to="/contact">Contact</Link>
+          </nav>
+          <h1>
+            I&apos;m a Michigan based Developer and Designer who specializes in
+            custom solutions for businesses including internal tooling a
+            customer facing applications.
+          </h1>
         </div>
-      </Home>
-    </Layout>
-  )
-}
+        <div className="image-wrap">
+          <svg
+            fill="none"
+            viewBox="0 0 657 549"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m271 163 386 107.89L380.6 549 271 163Z"
+              fill="#711CB4"
+              fillOpacity=".1"
+            />
+            <path fill="#53CAEE" fillOpacity=".1" d="M0 28h268v268H0z" />
+            <circle
+              cx="313.5"
+              cy="216.5"
+              fill="#F72585"
+              fillOpacity=".2"
+              r="216.5"
+            />
+          </svg>
+          <StaticImage
+            alt="Tom Gooden"
+            aspectRatio={1 / 1}
+            imgStyle={{
+              filter: `grayscale(100%) contrast(150%)`,
+            }}
+            placeholder="tracedSVG"
+            src="../assets/images/tom-gooden-web-design-development.png"
+            width={499}
+          />
+        </div>
+      </div>
+    </Home>
+  </Layout>
+)
 
 export default IndexPage

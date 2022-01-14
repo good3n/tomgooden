d988bd3b140e1e36e8b7b571b62f5b1036a671b2
Tom Gooden <good3n@gmail.com>
2022-01-13 23:43:36 -0500

    Remove unused import

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
-import { ServicesBlocks } from '../components/ServicesBlocks'
 import { ProjectRoadmap } from '../components/ProjectRoadmap'
 import styled from 'styled-components'
 
